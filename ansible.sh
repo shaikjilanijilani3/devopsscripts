67dcf5568baa479eceacf7923ff608e23ca2cdcf
Shaik Jilani <168560351+shaikjilanijilani3@users.noreply.github.com>
2025-12-10 12:31:42 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
-amazon-linux-extras install ansible2 -y
-yum install python3 python-pip python-dlevel -y
+amazon-linux-extras install ansible2 -y   (or)   dnf install ansible -y
+yum install python3 python-pip python-dlevel -y   (or)    dnf install -y python3 python3-pip python3-devel git sshpass
